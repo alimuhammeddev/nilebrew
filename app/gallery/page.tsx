8d500db535339c6dbb2e82761bb9d0a64e099f95
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-01-05 05:16:53 +0100

    created gallery section

---
+++ app/gallery/page.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "@/components/navbar";
+
+export default function Gallery() {
+    return (
+        <section>
+            <div>
+                <Navbar />
+            </div>
+        </section>
+    )
+}
