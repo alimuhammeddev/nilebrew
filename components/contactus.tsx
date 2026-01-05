c931985c7bf5578e71a820c2e8fbca9edf84a524
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-01-05 15:08:00 +0100

    commit

--- components/contactus.tsx
+++ components/contactus.tsx
@@ -5,7 +5,7 @@ export default function ContactUS() {
         <p className="lg:text-xl font-medium">Contact Us</p>
         <div className="w-28 h-0.5 bg-[#763919] mt-2"></div>
       </div>
-      <div className="flex lg:flex-row flex-col items-center mx-auto justify-between lg:gap-28 gap-5 mt-10">
+      <div className="flex lg:flex-row md:flex-row flex-col items-center mx-auto justify-between lg:gap-28 gap-5 mt-10">
         <div className="space-y-4">
           <h1 className="text-lg lg:text-2xl font-medium">
             Let's Brew Something Exceptional
