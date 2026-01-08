10a341f00795bae66517ca6df0dbe60d9b88929f
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-01-08 15:52:56 +0100

    commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -49,8 +49,8 @@ export default function Hero() {
 
         {/* Image Section */}
         <motion.div
-          initial={{ opacity: 0, x: 40 }}
-          animate={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
           className="flex justify-center lg:justify-end"
         >
