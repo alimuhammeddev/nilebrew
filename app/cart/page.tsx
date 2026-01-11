9a30b0051c60519a8af675a0e6a13a33f99034de
Ali Muhammed <muhammedaliali2020@gmail.com>
2026-01-11 16:34:16 +0100

    commit

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,16 +1,19 @@
 import CartPage from "@/components/cartpage";
 import { Navbar } from "@/components/navbar";
+import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
 
 export default function Cart() {
-    return (
-        <section>
-            <div>
-                <Navbar />
-            </div>
+  return (
+    <section>
+      <div>
+        <Navbar />
+      </div>
 
-            <div>
-                <CartPage />
-            </div>
-        </section>
-    )
+      <Suspense fallback={<div>Loading cart...</div>}>
+        <CartPage />
+      </Suspense>
+    </section>
+  );
 }
